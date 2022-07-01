fcadaec346e0865a59a247b7c8d57d6cf998dfd0
DM <dheemanthmanur72@gmail.com>
2022-07-02 02:14:11 +0530

    Implemented basic mechanism

--- src/Laminar.hs
+++ src/Laminar.hs
@@ -1,4 +1,66 @@
-module Laminar (someFunc) where
+module Laminar (Task (..), TaskGraph (..), buildGraph, runGraph) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Control.Concurrent (MVar, threadDelay)
+import Control.Concurrent.Async
+import Control.Monad (when)
+import Data.List
+import Data.Map.Strict (Map)
+import qualified Data.Map.Strict as Map
+import Data.Text (Text)
+import qualified Data.Text as Text
+import System.Random (randomRIO)
+
+-- types
+
+data Task = Task
+  { taskName :: Text,
+    taskDeps :: [Text],
+    taskAction :: IO ()
+  }
+
+data TaskGraph = TaskGraph
+  { graph :: Map Text [Text],
+    tasks :: [Task]
+  }
+
+-- Creating Taskgraphs
+
+buildGraph :: [Task] -> TaskGraph
+buildGraph ts = TaskGraph {graph = taskGraph, tasks = ts}
+  where
+    taskGraph = Map.fromList $ map (\x -> (taskName x, taskDeps x)) ts
+
+taskByName :: TaskGraph -> Text -> Task
+taskByName tg n = head $ filter (\x -> n == taskName x) (tasks tg)
+
+-- Running TaskGraphs
+
+runGraph :: TaskGraph -> IO ()
+runGraph tg = graphRunner tg []
+
+graphRunner :: TaskGraph -> [Async Text] -> IO ()
+graphRunner tg al =
+  do
+    let rt = runnable tg
+    -- print $ graph tg
+    ar <- mapM (async . runTask . taskByName tg) rt
+    let ual = al ++ ar
+    if null ual
+      then putStrLn "No more tasks to run."
+      else do
+        (t, tn) <- waitAny ual
+        let utg = foldl updateGraph tg rt
+        graphRunner utg (delete t ual)
+
+runTask :: Task -> IO Text
+runTask t = do
+  taskAction t
+  return $ taskName t
+
+updateGraph :: TaskGraph -> Text -> TaskGraph
+updateGraph (TaskGraph tg tl) tn = TaskGraph {graph = udg tg, tasks = tl}
+  where
+    udg = Map.map (delete tn) . Map.delete tn
+
+runnable :: TaskGraph -> [Text]
+runnable (TaskGraph tg _) = Map.keys $ Map.filter null tg
--- src/Laminar/Internal.hs
+++ src/Laminar/Internal.hs
@@ -1,102 +1,12 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module Laminar.Internal (buildGraph, runTaskGraph, taskList) where
+module Laminar.Internal (taskList) where
 
 import Control.Concurrent
-import Control.Exception (assert)
-import Control.Monad
-import Data.List
-import Data.Map.Strict (Map)
-import qualified Data.Map.Strict as Map
-import Data.Text (Text)
-import qualified Data.Text as Text
+import Laminar (Task (..))
 import System.Random
 
--- types
-data Task = Task
-  { taskName :: Text,
-    taskDeps :: [Text],
-    taskAction :: IO ()
-  }
-
-data TaskGraph = TaskGraph
-  { graph :: Map Text [Text],
-    tasks :: [Task]
-  }
-
--- Creating Taskgraphs
-
-buildGraph :: [Task] -> TaskGraph
-buildGraph ts = TaskGraph {graph = taskGraph, tasks = ts}
-  where
-    taskGraph = Map.fromList $ map (\x -> (taskName x, taskDeps x)) ts
-
-taskByName :: TaskGraph -> Text -> Task
-taskByName tg n = head $ filter (\x -> n == taskName x) (tasks tg)
-
--- Running TaskGraphs
-
-runTaskGraph :: TaskGraph -> IO ()
-runTaskGraph tg = do
-  endSig <- newEmptyMVar
-  runTaskGraph' tg endSig
-  takeMVar endSig
-
-runTaskGraph' :: TaskGraph -> MVar () -> IO ()
-runTaskGraph' tg endSig = do
-  stg <- newMVar etg
-  runTaskName stg startTask
-  where
-    (etg, startTask) = addDefaultTasks tg endSig
-
 -- Internal
-type SharedTaskGraph = MVar TaskGraph
-
-runTaskName :: SharedTaskGraph -> Text -> IO ()
-runTaskName stg n = do
-  tg <- readMVar stg
-  runTask stg (taskByName tg n)
-
-runTask :: SharedTaskGraph -> Task -> IO ()
-runTask stg t = void . forkIO $ do
-  updateTaskGraph stg (taskName t)
-  taskAction t
-  pollAndFork stg
-
-updateTaskGraph :: SharedTaskGraph -> Text -> IO ()
-updateTaskGraph stg t = modifyMVar_ stg update
-  where
-    update tg = return $ TaskGraph {graph = Map.map (delete t) (Map.delete t $ graph tg), tasks = tasks tg} -- use generic-lens
-
-pollAndFork :: SharedTaskGraph -> IO ()
-pollAndFork stg = do
-  tg <- readMVar stg
-  print (graph tg)
-  let ts = Map.filter null (graph tg)
-  mapM_ (runTask stg . taskByName tg) (Map.keys ts)
-
-addDefaultTasks :: TaskGraph -> MVar () -> (TaskGraph, Text)
-addDefaultTasks tg endSig = (tg'', sname)
-  where
-    tg'' = addEndTask tg' ename endSig
-    tg' = addStartTask tg sname
-    (sname, ename) = ("LaminarStart", "LaminarEnd")
-
--- getDefaultTaskNames :: TaskGraph -> (Text, Text)
--- getDefaultTaskNames = undefined
-
-addStartTask :: TaskGraph -> Text -> TaskGraph
-addStartTask (TaskGraph _ tl) name = buildGraph (startTask : tl)
-  where
-    startTask = Task {taskName = name, taskDeps = [], taskAction = putStrLn "Task LaminarStart Completed."}
-
-addEndTask :: TaskGraph -> Text -> MVar () -> TaskGraph
-addEndTask (TaskGraph _ tl) name endSig = buildGraph (endTask : tl)
-  where
-    endTask = Task {taskName = name, taskDeps = map taskName tl, taskAction = act}
-    act = do
-      putStrLn "Task LaminarEnd Completed."
-      putMVar endSig ()
 
 -- test data
 
@@ -110,12 +20,12 @@ taskA = Task "A" [] $ do
 
 taskB :: Task
 taskB = Task "B" ["A"] $ do
-  randomDelay
+  threadDelay $ 3 * (10 ^ 6)
   putStrLn "Task B Complete."
 
 taskC :: Task
 taskC = Task "C" ["A"] $ do
-  threadDelay (10 ^ 7)
+  threadDelay $ 1 * (10 ^ 6)
   putStrLn "Task C Complete."
 
 taskD :: Task
